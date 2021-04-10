da3dab3b3eadb03191bf468bd4fedcaf3314cee1
Samuel Sandoval <sjsandoval1000@gmail.com>
2021-04-09 21:36:15 -0700

    Initial Commit

---
+++ FunQuizApp/FunQuizAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FunQuizAppApp.swift
+//  FunQuizApp
+//
+//  Created by Samuel Sandoval on 4/9/21.
+//
+
+import SwiftUI
+
+@main
+struct FunQuizAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
